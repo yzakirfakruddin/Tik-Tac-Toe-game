5e43a6b52853ddfae31e6fde1ebfbe44da21ec05
yzakirfakruddin <zakiryadiki@gmail.com>
2024-08-16 15:50:57 +0530

    responsive for mobile screen

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,11 +1,14 @@
 export default function Logs({ turns }) {
   return (
-    <ol id="log">
-      {turns.map((turn) => (
-        <li key={`${turn.square.row}${turn.square.col}`}>
-          {turn.player} selected {turn.square.row},{turn.square.col}
-        </li>
-      ))}
-    </ol>
+    <>
+      <ol id="log">
+        <h2>Player Logs</h2>
+        {turns.map((turn) => (
+          <li key={`${turn.square.row}${turn.square.col}`}>
+            {turn.player} selected {turn.square.row},{turn.square.col}
+          </li>
+        ))}
+      </ol>
+    </>
   );
 }
